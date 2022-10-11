be400b1506d606746722b7ef67355a9052074749
Shahzaib Ali <shahzaibalifakhr@hotmail.com>
2022-10-11 15:04:35 +0500

    latest code

--- app/Http/Controllers/GoogleDriveController.php
+++ app/Http/Controllers/GoogleDriveController.php
@@ -7,76 +7,67 @@ use App\Models\User;
 
 class GoogleDriveController extends Controller
 {
-    public $gClient;
+    public $client;
+
     public function __construct(){
         $google_redirect_url = url('google/login');
-        $this->gClient = new \Google_Client();
-        $this->gClient->setApplicationName('Demo App');
-        $this->gClient->setClientId(env('GOOGLE_CLIENT_ID'));
-        $this->gClient->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
-        $this->gClient->setRedirectUri($google_redirect_url);
-        $this->gClient->setDeveloperKey(env('GOOGLE_API_KEY'));
-        $this->gClient->setScopes(array(               
+        $this->client = new \Google_Client();
+        $this->client->setApplicationName('Demo App');
+        $this->client->setClientId(env('GOOGLE_CLIENT_ID'));
+        $this->client->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
+        $this->client->setRedirectUri($google_redirect_url);
+        $this->client->setDeveloperKey(env('GOOGLE_API_KEY'));
+        $this->client->setScopes(array(               
             'https://www.googleapis.com/auth/drive.file',
             'https://www.googleapis.com/auth/drive',
             // 'https://www.googleapis.com/auth/userinfo.email',
             // 'https://www.googleapis.com/auth/userinfo.profile'
         ));
-        $this->gClient->setAccessType("offline");
-        $this->gClient->setApprovalPrompt("force");
+        $this->client->setAccessType("offline");
+        $this->client->setApprovalPrompt("force");
     }
 
-    public function index()
-    {
+    public function index(){
         return view('drive');
     }
 
     public function login(Request $request)  {
-        
-        $google_oauthV2 = new \Google_Service_Oauth2($this->gClient);
-        if ($request->get('code')){
-            $this->gClient->authenticate($request->get('code'));
-            $request->session()->put('token', $this->gClient->getAccessToken());
+        $google_oauthV2 = new \Google_Service_Oauth2($this->client);
+
+        if($request->get('code')){
+            $this->client->authenticate($request->get('code'));
+            $request->session()->put('token', $this->client->getAccessToken());
         }
-        if ($request->session()->get('token'))
-        {
-            $this->gClient->setAccessToken($request->session()->get('token'));
+
+        if ($request->session()->get('token')){
+            $this->client->setAccessToken($request->session()->get('token'));
         }
-        if ($this->gClient->getAccessToken())
-        {
-            //For logged in user, get details from google using acces
+
+        if ($this->client->getAccessToken()){
             $user = User::find(1);
             $user->access_token = json_encode($request->session()->get('token'));
             $user->save();
             dd("Successfully Authenticated");
-        } else
-        {
-            //For Guest user, get google login url
-            $authUrl = $this->gClient->createAuthUrl();
+        }else{
+            $authUrl = $this->client->createAuthUrl();
             return redirect()->to($authUrl);
         }
     }
-    public function upload(Request $request){
-        $service = new \Google_Service_Drive($this->gClient);
-        $user = User::find(1);
-        $this->gClient->setAccessToken(json_decode($user->access_token,true));
 
-        if ($this->gClient->isAccessTokenExpired()){
-            // save refresh token to some variable
-            $refreshTokenSaved = $this->gClient->getRefreshToken();
-            // update access token
-            $this->gClient->fetchAccessTokenWithRefreshToken($refreshTokenSaved);               
-            // // pass access token to some variable
-            $updatedAccessToken = $this->gClient->getAccessToken();
-            // // append refresh token
+    public function upload(Request $request){
+        $service = new \Google_Service_Drive($this->client);
+        $user = User::find(1);
+
+        $this->client->setAccessToken(json_decode($user->access_token,true));
+        if ($this->client->isAccessTokenExpired()){
+            $refreshTokenSaved = $this->client->getRefreshToken();
+            $this->client->fetchAccessTokenWithRefreshToken($refreshTokenSaved);
+            $updatedAccessToken = $this->client->getAccessToken();
             $updatedAccessToken['refresh_token'] = $refreshTokenSaved;
-            //Set the new acces token
-            $this->gClient->setAccessToken($updatedAccessToken);
-            
+            $this->client->setAccessToken($updatedAccessToken);
             $user->access_token=$updatedAccessToken;
             $user->save();                
         }
-
 
         $folderName = 'Images';
         $mainfolderId = '1ejOHGDDKBoyTZH4-g-c_S7vWO-u2iEh4';
@@ -88,7 +79,7 @@ class GoogleDriveController extends Controller
             $f->setMimeType('application/vnd.google-apps.folder');
             $f->setParents(array($mainfolderId));
             $folderId = $service->files->create($f)->getId();
-        }else {
+        }else{
             $folderId = $folders->getFiles()[0]->getId();
         }
         
@@ -104,7 +95,6 @@ class GoogleDriveController extends Controller
           'uploadType' => 'media'
         ));
 
-        // get url of uploaded file
         $url='https://drive.google.com/open?id='.$result->id;
         dd($url);
     }
